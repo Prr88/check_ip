db29748f38a42b0a490d675f3b0301d3e7dad86a
Prr88 <64741421+Prr88@users.noreply.github.com>
2020-05-03 16:06:15 +0000

    Create check_exit_node.sh

---
+++ check_exit_node.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+function notify {
+	if [ -e /usr/bin/notify-send ]; then
+		/usr/bin/notify-send "Your public IP is:" "$1"
+	fi
+}
+
+flagToFind=`whois $(curl ifconfig.me)|grep country|sed 's/country:'//|sort --unique|sed -e 's/\(.*\)/\L\1/'`
+flagToShow=`cd /usr/share/flags/countries/16x11/;ls|grep $flagToFind`
+MYIP=`whois $(curl ifconfig.me)|grep -E -i "cidr|country|city|address|organization|descr"|sort`
+notify "$MYIP\npuplic IP:      $(curl ifconfig.me)"
+sleep 1.0
+notify-send "is your exit node " -i /usr/share/flags/countries/16x11/$flagToShow    
